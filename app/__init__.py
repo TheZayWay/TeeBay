bec311e6a0b8f83e95879bc3a011d17bfddf8f52
isaiah <isaiahcodesforgood@gmail.com>
2023-07-17 10:20:28 -0700

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -18,9 +18,6 @@ app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 login = LoginManager(app)
 login.login_view = 'auth.unauthorized'
 
-@app.route('/')
-def index(): 
-    return "<h1>Home<h1>"
 
 @login.user_loader
 def load_user(id):
